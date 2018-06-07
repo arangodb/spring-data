256cad69dc3b1614e8ede9e91ec228360222bc33
Christian Lechner <6638938+christian-lechner@users.noreply.github.com>
2018-06-07 16:34:32 +0200

    add test (#59)

---
+++ src/test/java/com/arangodb/springframework/testdata/Role.java
@@ -0,0 +1,69 @@
+/*
+ * DISCLAIMER
+ *
+ * Copyright 2018 ArangoDB GmbH, Cologne, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright holder is ArangoDB GmbH, Cologne, Germany
+ */
+
+package com.arangodb.springframework.testdata;
+
+import org.springframework.data.annotation.Id;
+
+import com.arangodb.springframework.annotation.Edge;
+import com.arangodb.springframework.annotation.From;
+import com.arangodb.springframework.annotation.To;
+
+/**
+ * 
+ * @author Christian Lechner
+ */
+@Edge("roles")
+public class Role {
+
+	@Id
+	private String id;
+
+	@From(lazy = true)
+	private Actor actor;
+
+	@To
+	private Movie movie;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Actor getActor() {
+		return actor;
+	}
+
+	public void setActor(Actor actor) {
+		this.actor = actor;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+}
