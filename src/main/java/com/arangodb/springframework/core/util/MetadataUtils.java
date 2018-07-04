8f2046f9f88ba56c3b5a984a60ff910913ab6889
Christian Lechner <6638938+christian-lechner@users.noreply.github.com>
2018-07-04 16:03:15 +0200

    support @Key fields on references (#93)

--- src/main/java/com/arangodb/springframework/core/util/MetadataUtils.java
+++ src/main/java/com/arangodb/springframework/core/util/MetadataUtils.java
@@ -7,29 +7,50 @@ package com.arangodb.springframework.core.util;
  * Generic utilities for metadata, such as records' _keys.
  * 
  * @author Reşat SABIQ
+ * @author Christian Lechner
  */
-public class MetadataUtils {
+public final class MetadataUtils {
+
 	public static final char KEY_DELIMITER = '/';
 
-	private MetadataUtils() {}
-	
+	private MetadataUtils() {
+	}
+
 	/**
 	 * Provides a substring with _key.
-	 * @param id	string consisting of concatenation of collection name, /, & _key.
-	 * @return	_key
+	 * 
+	 * @param id
+	 *            string consisting of concatenation of collection name, /, & _key.
+	 * @return _key
 	 */
 	public static String determineDocumentKeyFromId(final String id) {
 		final int lastSlash = id.lastIndexOf(KEY_DELIMITER);
-		return id.substring(lastSlash+1);
+		return id.substring(lastSlash + 1);
 	}
-	
+
 	/**
 	 * Provides a substring with collection name.
-	 * @param id	string consisting of concatenation of collection name, /, & _key.
-	 * @return	collection name (or null if no key delimiter is present)
+	 * 
+	 * @param id
+	 *            string consisting of concatenation of collection name, /, & _key.
+	 * @return collection name (or null if no key delimiter is present)
 	 */
 	public static String determineCollectionFromId(final String id) {
 		final int delimiter = id.indexOf(KEY_DELIMITER);
 		return delimiter == -1 ? null : id.substring(0, delimiter);
 	}
+
+	/**
+	 * Creates an _id from collection name and _key.
+	 * 
+	 * @param collectionName
+	 *            the name of a collection
+	 * @param key
+	 *            the _key of a document
+	 * @return _id
+	 */
+	public static String createIdFromCollectionAndKey(final String collectionName, final String key) {
+		return collectionName + KEY_DELIMITER + key;
+	}
+
 }
