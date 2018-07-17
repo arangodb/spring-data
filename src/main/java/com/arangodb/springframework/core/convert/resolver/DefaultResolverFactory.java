6931894421b0a7da46fb63df90c58634eed8eb2f
Christian Lechner <christian.lechner@live.de>
2018-07-17 09:07:47 +0200

    add disclaimer

--- src/main/java/com/arangodb/springframework/core/convert/resolver/DefaultResolverFactory.java
+++ src/main/java/com/arangodb/springframework/core/convert/resolver/DefaultResolverFactory.java
@@ -1,3 +1,23 @@
+/*
+ * DISCLAIMER
+ *
+ * Copyright 2018 ArangoDB GmbH, Cologne, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright holder is ArangoDB GmbH, Cologne, Germany
+ */
+
 package com.arangodb.springframework.core.convert.resolver;
 
 import java.lang.annotation.Annotation;
@@ -9,6 +29,11 @@ import com.arangodb.springframework.annotation.Relations;
 import com.arangodb.springframework.annotation.To;
 import com.arangodb.springframework.core.ArangoOperations;
 
+/**
+ * 
+ * @author Mark Vollmary
+ * @author Christian Lechner
+ */
 public class DefaultResolverFactory implements ResolverFactory {
 
 	private final RefResolver refResolver;
