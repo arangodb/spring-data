4ae3130af345a0314a215605aa38fb7c88d41d5b
Mark Vollmary <mpv1989@users.noreply.github.com>
2018-11-09 15:20:49 +0100

    Add interface ArangoConfiguration to replace AbstractArangoConfiguration (#129)

--- src/main/java/com/arangodb/springframework/config/AbstractArangoConfiguration.java
+++ src/main/java/com/arangodb/springframework/config/AbstractArangoConfiguration.java
@@ -20,149 +20,16 @@
 
 package com.arangodb.springframework.config;
 
-import java.lang.annotation.Annotation;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.data.convert.CustomConversions;
-import org.springframework.data.mapping.model.FieldNamingStrategy;
-import org.springframework.data.mapping.model.PropertyNameFieldNamingStrategy;
-
-import com.arangodb.ArangoDB;
-import com.arangodb.ArangoDBException;
-import com.arangodb.springframework.annotation.Document;
-import com.arangodb.springframework.annotation.Edge;
-import com.arangodb.springframework.annotation.From;
-import com.arangodb.springframework.annotation.Ref;
-import com.arangodb.springframework.annotation.Relations;
-import com.arangodb.springframework.annotation.To;
-import com.arangodb.springframework.core.ArangoOperations;
-import com.arangodb.springframework.core.convert.ArangoConverter;
-import com.arangodb.springframework.core.convert.ArangoCustomConversions;
-import com.arangodb.springframework.core.convert.ArangoTypeMapper;
-import com.arangodb.springframework.core.convert.DefaultArangoConverter;
-import com.arangodb.springframework.core.convert.DefaultArangoTypeMapper;
-import com.arangodb.springframework.core.convert.resolver.DocumentFromResolver;
-import com.arangodb.springframework.core.convert.resolver.DocumentToResolver;
-import com.arangodb.springframework.core.convert.resolver.EdgeFromResolver;
-import com.arangodb.springframework.core.convert.resolver.EdgeToResolver;
-import com.arangodb.springframework.core.convert.resolver.RefResolver;
-import com.arangodb.springframework.core.convert.resolver.ReferenceResolver;
-import com.arangodb.springframework.core.convert.resolver.RelationResolver;
-import com.arangodb.springframework.core.convert.resolver.RelationsResolver;
-import com.arangodb.springframework.core.convert.resolver.ResolverFactory;
-import com.arangodb.springframework.core.mapping.ArangoMappingContext;
-import com.arangodb.springframework.core.template.ArangoTemplate;
 
 /**
  * @author Mark Vollmary
  * @author Christian Lechner
- *
+ * @deprecated {@link ArangoConfiguration} provides default methods and can be
+ *             implemented directly without the need of this class.
  */
 @Configuration
-public abstract class AbstractArangoConfiguration {
-
-	protected abstract ArangoDB.Builder arango();
-
-	protected abstract String database();
-
-	@Bean
-	public ArangoOperations arangoTemplate() throws Exception {
-		return new ArangoTemplate(arango().build(), database(), arangoConverter());
-	}
-
-	@Bean
-	public ArangoMappingContext arangoMappingContext() throws Exception {
-		final ArangoMappingContext context = new ArangoMappingContext();
-		context.setInitialEntitySet(getInitialEntitySet());
-		context.setFieldNamingStrategy(fieldNamingStrategy());
-		context.setSimpleTypeHolder(customConversions().getSimpleTypeHolder());
-		return context;
-	}
-
-	@Bean
-	public ArangoConverter arangoConverter() throws Exception {
-		return new DefaultArangoConverter(arangoMappingContext(), customConversions(), resolverFactory(),
-				arangoTypeMapper());
-	}
-
-	protected CustomConversions customConversions() {
-		return new ArangoCustomConversions(customConverters());
-	}
-
-	protected Collection<Converter<?, ?>> customConverters() {
-		return Collections.emptyList();
-	}
-
-	private Set<? extends Class<?>> getInitialEntitySet() throws ClassNotFoundException {
-		return ArangoEntityClassScanner.scanForEntities(getEntityBasePackages());
-	}
-
-	protected String[] getEntityBasePackages() {
-		return new String[] { getClass().getPackage().getName() };
-	}
-
-	protected FieldNamingStrategy fieldNamingStrategy() {
-		return PropertyNameFieldNamingStrategy.INSTANCE;
-	}
-
-	protected String typeKey() {
-		return DefaultArangoTypeMapper.DEFAULT_TYPE_KEY;
-	}
-
-	protected ArangoTypeMapper arangoTypeMapper() throws Exception {
-		return new DefaultArangoTypeMapper(typeKey(), arangoMappingContext());
-	}
-
-	protected ResolverFactory resolverFactory() {
-		return new ResolverFactory() {
-			@SuppressWarnings("unchecked")
-			@Override
-			public <A extends Annotation> Optional<ReferenceResolver<A>> getReferenceResolver(final A annotation) {
-				ReferenceResolver<A> resolver = null;
-				try {
-					if (annotation instanceof Ref) {
-						resolver = (ReferenceResolver<A>) new RefResolver(arangoTemplate());
-					}
-				} catch (final Exception e) {
-					throw new ArangoDBException(e);
-				}
-				return Optional.ofNullable(resolver);
-			}
-
-			@SuppressWarnings("unchecked")
-			@Override
-			public <A extends Annotation> Optional<RelationResolver<A>> getRelationResolver(
-				final A annotation,
-				final Class<? extends Annotation> collectionType) {
-				RelationResolver<A> resolver = null;
-				try {
-					if (annotation instanceof From) {
-						if (collectionType == Edge.class) {
-							resolver = (RelationResolver<A>) new EdgeFromResolver(arangoTemplate());
-						} else if (collectionType == Document.class) {
-							resolver = (RelationResolver<A>) new DocumentFromResolver(arangoTemplate());
-						}
-					} else if (annotation instanceof To) {
-						if (collectionType == Edge.class) {
-							resolver = (RelationResolver<A>) new EdgeToResolver(arangoTemplate());
-						} else if (collectionType == Document.class) {
-							resolver = (RelationResolver<A>) new DocumentToResolver(arangoTemplate());
-						}
-					} else if (annotation instanceof Relations) {
-						resolver = (RelationResolver<A>) new RelationsResolver(arangoTemplate());
-					}
-				} catch (final Exception e) {
-					throw new ArangoDBException(e);
-				}
-				return Optional.ofNullable(resolver);
-			}
-		};
-	}
+@Deprecated
+public abstract class AbstractArangoConfiguration implements ArangoConfiguration {
 
 }
