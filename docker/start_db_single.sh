b8e3e29142171a126c2ab9877e83d276a8ae072a
Michele Rastelli <rashtao@gmail.com>
2020-08-11 20:32:41 +0200

    retry on cluster startup failure

--- docker/start_db_single.sh
+++ docker/start_db_single.sh
@@ -5,11 +5,13 @@
 #   ./start_db_single.sh <dockerImage>
 
 # EXAMPLE:
-#   ./start_db_single.sh docker.io/arangodb/arangodb:3.6.2
+#   ./start_db_single.sh docker.io/arangodb/arangodb:3.7.1
 
 docker pull "$1"
 
-docker run -d -e ARANGO_ROOT_PASSWORD=test -e ARANGO_LICENSE_KEY="$ARANGO_LICENSE_KEY" -p 8529:8529 "$1"
+docker network create arangodb --subnet 172.28.0.0/16
+
+docker run -d -p 8529:8529 -e ARANGO_ROOT_PASSWORD=test -e ARANGO_LICENSE_KEY="$ARANGO_LICENSE_KEY" --network arangodb --ip 172.28.3.1 --name arangodb "$1"
 
 echo "waiting for arangodb ..."
 
