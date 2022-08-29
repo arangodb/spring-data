38ab4f6457e1b93c0f1ee7f6ba57f55a99de9ec1
Arne Burmeister <arne.burmeister@wps.de>
2022-08-29 12:58:08 +0200

    #80 extract default resolver factory to allow bean dependency, pass query bridge if available

--- src/main/java/com/arangodb/springframework/config/ArangoConfiguration.java
+++ src/main/java/com/arangodb/springframework/config/ArangoConfiguration.java
@@ -4,10 +4,8 @@
 package com.arangodb.springframework.config;
 
 import java.io.IOException;
-import java.lang.annotation.Annotation;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Optional;
 import java.util.Set;
 
 import com.arangodb.ContentType;
@@ -23,27 +21,13 @@ import org.springframework.data.mapping.model.FieldNamingStrategy;
 import org.springframework.data.mapping.model.PropertyNameFieldNamingStrategy;
 
 import com.arangodb.ArangoDB;
-import com.arangodb.ArangoDBException;
-import com.arangodb.springframework.annotation.Document;
-import com.arangodb.springframework.annotation.Edge;
-import com.arangodb.springframework.annotation.From;
-import com.arangodb.springframework.annotation.Ref;
-import com.arangodb.springframework.annotation.Relations;
-import com.arangodb.springframework.annotation.To;
 import com.arangodb.springframework.core.ArangoOperations;
 import com.arangodb.springframework.core.convert.ArangoConverter;
 import com.arangodb.springframework.core.convert.ArangoCustomConversions;
 import com.arangodb.springframework.core.convert.ArangoTypeMapper;
 import com.arangodb.springframework.core.convert.DefaultArangoConverter;
 import com.arangodb.springframework.core.convert.DefaultArangoTypeMapper;
-import com.arangodb.springframework.core.convert.resolver.DocumentFromResolver;
-import com.arangodb.springframework.core.convert.resolver.DocumentToResolver;
-import com.arangodb.springframework.core.convert.resolver.EdgeFromResolver;
-import com.arangodb.springframework.core.convert.resolver.EdgeToResolver;
-import com.arangodb.springframework.core.convert.resolver.RefResolver;
-import com.arangodb.springframework.core.convert.resolver.ReferenceResolver;
-import com.arangodb.springframework.core.convert.resolver.RelationResolver;
-import com.arangodb.springframework.core.convert.resolver.RelationsResolver;
+import com.arangodb.springframework.core.convert.resolver.DefaultResolverFactory;
 import com.arangodb.springframework.core.convert.resolver.ResolverFactory;
 import com.arangodb.springframework.core.mapping.ArangoMappingContext;
 import com.arangodb.springframework.core.template.ArangoTemplate;
@@ -156,49 +140,11 @@ public interface ArangoConfiguration {
         return new DefaultArangoTypeMapper(typeKey(), arangoMappingContext());
     }
 
+	@Bean
     default ResolverFactory resolverFactory() {
-        return new ResolverFactory() {
-            @SuppressWarnings("unchecked")
-            @Override
-            public <A extends Annotation> Optional<ReferenceResolver<A>> getReferenceResolver(final A annotation) {
-                ReferenceResolver<A> resolver = null;
-                try {
-                    if (annotation instanceof Ref) {
-						resolver = (ReferenceResolver<A>) new RefResolver(arangoTemplate(), null);
-                    }
-                } catch (final Exception e) {
-                    throw new ArangoDBException(e);
-                }
-                return Optional.ofNullable(resolver);
+		return RESOLVER_FACTORY_INSTANCE;
             }
 
-            @SuppressWarnings("unchecked")
-            @Override
-            public <A extends Annotation> Optional<RelationResolver<A>> getRelationResolver(final A annotation,
-                                                                                            final Class<? extends Annotation> collectionType) {
-                RelationResolver<A> resolver = null;
-                try {
-                    if (annotation instanceof From) {
-                        if (collectionType == Edge.class) {
-							resolver = (RelationResolver<A>) new EdgeFromResolver(arangoTemplate(), null);
-                        } else if (collectionType == Document.class) {
-							resolver = (RelationResolver<A>) new DocumentFromResolver(arangoTemplate(), null);
-                        }
-                    } else if (annotation instanceof To) {
-                        if (collectionType == Edge.class) {
-							resolver = (RelationResolver<A>) new EdgeToResolver(arangoTemplate(), null);
-                        } else if (collectionType == Document.class) {
-							resolver = (RelationResolver<A>) new DocumentToResolver(arangoTemplate(), null);
-                        }
-                    } else if (annotation instanceof Relations) {
-						resolver = (RelationResolver<A>) new RelationsResolver(arangoTemplate(), null);
-                    }
-                } catch (final Exception e) {
-                    throw new ArangoDBException(e);
-                }
-                return Optional.ofNullable(resolver);
-            }
-        };
-    }
+	ResolverFactory RESOLVER_FACTORY_INSTANCE = new DefaultResolverFactory();
 
 }
