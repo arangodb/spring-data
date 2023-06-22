2e26a382a3474261936b1947d65265647b869bba
sivaprasad <38925188+galaxyse@users.noreply.github.com>
2023-06-22 13:35:17 -0700

    Add filter and Search support
    Filter and search support

--- src/main/java/com/arangodb/springframework/repository/query/ArangoParameters.java
+++ src/main/java/com/arangodb/springframework/repository/query/ArangoParameters.java
@@ -2,6 +2,7 @@
  * DISCLAIMER
  *
  * Copyright 2017 ArangoDB GmbH, Cologne, Germany
+ * Copyright 2023 Hewlett Packard Enterprise Development LP.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +17,7 @@
  * limitations under the License.
  *
  * Copyright holder is ArangoDB GmbH, Cologne, Germany
+ * Copyright holder is Hewlett Packard Enterprise Development LP.
  */
 
 package com.arangodb.springframework.repository.query;
@@ -23,6 +25,7 @@ package com.arangodb.springframework.repository.query;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -36,6 +39,9 @@ import org.springframework.core.MethodParameter;
 import org.springframework.data.repository.query.Parameter;
 import org.springframework.data.repository.query.Parameters;
 import org.springframework.util.Assert;
+import com.arangodb.springframework.annotation.Filter;
+import com.arangodb.springframework.repository.query.filter.Filterable;
+import com.arangodb.springframework.repository.query.search.Searchable;
 
 import com.arangodb.model.AqlQueryOptions;
 import com.arangodb.springframework.annotation.BindVars;
@@ -46,11 +52,14 @@ import com.arangodb.springframework.annotation.BindVars;
  * @author Mark McCormick
  * @author Mark Vollmary
  * @author Christian Lechner
+ * @author Siva Prasad Erigineni
  */
 public class ArangoParameters extends Parameters<ArangoParameters, ArangoParameters.ArangoParameter> {
 
 	private final int queryOptionsIndex;
 	private final int bindVarsIndex;
+	private final Map<String, Integer> filterIndexes;
+	private final int searchIndex;
 
 	public ArangoParameters(final Method method) {
 		super(method);
@@ -61,6 +70,9 @@ public class ArangoParameters extends Parameters<ArangoParameters, ArangoParamet
 		assertNonDuplicateParamNames(method);
 		this.queryOptionsIndex = getIndexOfSpecialParameter(ArangoParameter::isQueryOptions);
 		this.bindVarsIndex = getIndexOfSpecialParameter(ArangoParameter::isBindVars);
+		this.filterIndexes = mapFilterIndexes();
+		this.searchIndex = getIndexOfSpecialParameter(ArangoParameter::isSearch);
+
 	}
 
 	private ArangoParameters(final List<ArangoParameter> parameters, final int queryOptionsIndex,
@@ -68,6 +80,8 @@ public class ArangoParameters extends Parameters<ArangoParameters, ArangoParamet
 		super(parameters);
 		this.queryOptionsIndex = queryOptionsIndex;
 		this.bindVarsIndex = bindVarsIndex;
+		this.filterIndexes = filterIndexes;
+		this.searchIndex = searchIndex;
 	}
 
 	@Override
@@ -77,7 +91,7 @@ public class ArangoParameters extends Parameters<ArangoParameters, ArangoParamet
 
 	@Override
 	protected ArangoParameters createFrom(final List<ArangoParameter> parameters) {
-		return new ArangoParameters(parameters, this.queryOptionsIndex, this.bindVarsIndex);
+		return new ArangoParameters(parameters, this.queryOptionsIndex, this.bindVarsIndex, this.filterIndexes, this.searchIndex);
 	}
 
 	public boolean hasQueryOptions() {
@@ -95,6 +109,9 @@ public class ArangoParameters extends Parameters<ArangoParameters, ArangoParamet
 	public int getBindVarsIndex() {
 		return this.bindVarsIndex;
 	}
+	public boolean hasFilterParameter() { return this.filterIndexes.size() > 0; }
+
+	public Map<String, Integer> getFilterIndexes() { return this.filterIndexes; }
 
 	private int getIndexOfSpecialParameter(final Predicate<ArangoParameter> condition) {
 		for (int index = 0; index < getNumberOfParameters(); ++index) {
@@ -105,6 +122,29 @@ public class ArangoParameters extends Parameters<ArangoParameters, ArangoParamet
 		}
 		return -1;
 	}
+
+	public boolean hasSearchParameter() { return this.searchIndex != -1; }
+
+	public int getSearchIndex() {
+		return this.searchIndex;
+	}
+
+	/**
+	 * Returns the index of all Filter parameters indexed by their corresponding placeholders.
+	 */
+	private Map<String, Integer> mapFilterIndexes() {
+		Map<String, Integer> filters = new HashMap<>();
+
+		for (int index = 0; index < getNumberOfParameters(); ++index) {
+			final ArangoParameter param = getParameter(index);
+			if (param.isFilter()) {
+				filters.put(param.getPlaceholder(), index);
+			}
+		}
+
+		return filters;
+	}
+
 
 	private void assertSingleSpecialParameter(final Predicate<ArangoParameter> condition, final String message) {
 		boolean found = false;
@@ -145,6 +185,7 @@ public class ArangoParameters extends Parameters<ArangoParameters, ArangoParamet
 			this.parameter = parameter;
 			assertCorrectBindParamPattern();
 			assertCorrectBindVarsType();
+			assertFilterType();
 		}
 
 		@Override
@@ -163,6 +204,11 @@ public class ArangoParameters extends Parameters<ArangoParameters, ArangoParamet
 		public boolean isSpelParam() {
 			return parameter.hasParameterAnnotation(SpelParam.class);
 		}
+		public boolean isFilter() { return parameter.hasParameterAnnotation(Filter.class); }
+
+		public boolean isSearch() {
+			return Searchable.class.isAssignableFrom(parameter.getParameterType());
+		}
 
         @Override
         public Optional<String> getName() {
@@ -176,7 +222,11 @@ public class ArangoParameters extends Parameters<ArangoParameters, ArangoParamet
 		public String getPlaceholder() {
 			if (isNamedParameter()) {
 				return String.format(NAMED_PARAMETER_TEMPLATE, getName().get());
-			} else {
+			} 
+			else if (isFilter()) {
+				return this.parameter.getParameterAnnotation(Filter.class).value();
+			}  
+			else {
 				return String.format(POSITION_PARAMETER_TEMPLATE, getIndex());
 			}
 		}
@@ -210,6 +260,14 @@ public class ArangoParameters extends Parameters<ArangoParameters, ArangoParamet
 				Assert.isTrue(Class.class.isInstance(valueType), errorMsg);
 				Assert.isTrue(String.class.equals(keyType), errorMsg);
 				Assert.isTrue(Object.class.equals(valueType), errorMsg);
+			}
+		}
+		private void assertFilterType() {
+			final String errorMsg = "@Filter parameter must be of type Filterable! Offending parameter: parameter "
+					+ parameter.getParameterIndex() + " on method" + parameter.getMethod();
+
+			if (isFilter()) {
+				Assert.isTrue(Filterable.class.equals(parameter.getParameterType()), errorMsg);
 			}
 		}
 
