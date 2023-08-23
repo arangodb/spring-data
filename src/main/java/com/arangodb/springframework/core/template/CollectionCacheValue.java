3a665b6a568681c68dd03e68c77e5d1a8cae9938
Arne Burmeister <arne.burmeister@wps.de>
2023-08-23 18:00:57 +0200

    less ensure index calls

--- src/main/java/com/arangodb/springframework/core/template/CollectionCacheValue.java
+++ src/main/java/com/arangodb/springframework/core/template/CollectionCacheValue.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import com.arangodb.ArangoCollection;
+import com.arangodb.entity.IndexEntity;
 
 class CollectionCacheValue {
 
 	private final ArangoCollection collection;
 	private final Collection<Class<?>> entities;
+	private final Collection<IndexEntity> indexes;
 
 	public CollectionCacheValue(final ArangoCollection collection) {
 		super();
 		this.collection = collection;
 		this.entities = new ArrayList<>();
+		this.indexes = collection.getIndexes();
 	}
 
 	public ArangoCollection getCollection() {
@@ -22,6 +25,10 @@ class CollectionCacheValue {
 
 	public Collection<Class<?>> getEntities() {
 		return entities;
+	}
+
+	public Collection<IndexEntity> getIndexes() {
+		return indexes;
 	}
 
 	public void addEntityClass(final Class<?> entityClass) {
