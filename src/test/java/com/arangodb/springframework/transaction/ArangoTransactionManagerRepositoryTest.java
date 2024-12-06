06d36f5e8fece1012d4fd630342850f7479d6180
Arne Burmeister <arne.burmeister@wps.de>
2024-12-06 16:46:00 +0100

    #80 make count and deleteAll transactional

--- src/test/java/com/arangodb/springframework/transaction/ArangoTransactionManagerRepositoryTest.java
+++ src/test/java/com/arangodb/springframework/transaction/ArangoTransactionManagerRepositoryTest.java
@@ -5,7 +5,6 @@ import com.arangodb.springframework.ArangoTransactionalTestConfiguration;
 import com.arangodb.springframework.repository.ActorRepository;
 import com.arangodb.springframework.repository.MovieRepository;
 import com.arangodb.springframework.testdata.Movie;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Streamable;
@@ -84,7 +83,7 @@ class ArangoTransactionManagerRepositoryTest extends AbstractArangoTest {
 		List<Movie> list = Streamable.of(movieRepository.findAllById(List.of(saved.getId()))).toList();
 
 		assertThat(list).hasSize(1);
-		assertThat(list.getFirst().getId()).isEqualTo(saved.getId());
+		assertThat(list.get(0).getId()).isEqualTo(saved.getId());
 	}
 
 	@Test
@@ -110,7 +109,7 @@ class ArangoTransactionManagerRepositoryTest extends AbstractArangoTest {
 	@Test
 	@Transactional
 	public void shouldFindSavedEntityWithinTransaction_saveAll() {
-		Movie saved = Streamable.of(movieRepository.saveAll(List.of(starWars))).stream().toList().getFirst();
+		Movie saved = Streamable.of(movieRepository.saveAll(List.of(starWars))).stream().toList().get(0);
 
 		Boolean exists = movieRepository.existsById(saved.getId());
 
@@ -125,12 +124,11 @@ class ArangoTransactionManagerRepositoryTest extends AbstractArangoTest {
 		List<Movie> list = Streamable.of(movieRepository.findAll()).toList();
 
 		assertThat(list).hasSize(1);
-		assertThat(list.getFirst().getId()).isEqualTo(saved.getId());
+		assertThat(list.get(0).getId()).isEqualTo(saved.getId());
 	}
 
 	@Test
 	@Transactional
-	@Disabled("count is not transactional")
 	public void shouldFindSavedEntityWithinTransaction_count() {
 		movieRepository.save(starWars);
 
@@ -185,7 +183,6 @@ class ArangoTransactionManagerRepositoryTest extends AbstractArangoTest {
 
 	@Test
 	@Transactional
-	@Disabled("delete all is not transactional")
 	public void shouldFindSavedEntityWithinTransaction_deleteAllNoArg() {
 		Movie saved = movieRepository.save(starWars);
 
